84793709430b032c8298737b5e07b17bdd96fb8f
fujl <fujl1991@sina.com>
2018-09-26 21:09:10 +0800

    日常更新

---
+++ eil-project-dev/src/main/java/com/shencai/eil/gis/model/GisValueVO.java
@@ -0,0 +1,19 @@
+package com.shencai.eil.gis.model;
+
+import lombok.Data;
+
+/**
+ * @program: eil-project
+ * @description:
+ * @author: fujl
+ * @create: 2018-09-26 16:27
+ **/
+@Data
+public class GisValueVO {
+
+    private String entId;
+
+    private String latitude;
+
+    private String longitude;
+}
